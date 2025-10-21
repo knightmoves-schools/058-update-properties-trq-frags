73ec91a72378026f24283cc00da755279d8f1739
track-frags <jadirks@anamosa.k12.ia.us>
2025-10-21 18:12:52 +0000

    sfgfskjbgfdjg jjkfsg sfekj fdgfdk vbhjsdfbvf ;gkjfse bhjdfsbkdf bsfd bverj bfd her vubih ibfhg bre hbusrbhuerb srb ihrb sreb serb rb drbar gioe ght4hub e09 544bhw54 gh54r g9b34wg rn3q girwgwr9 8uherw gherer9g herwgue g9rew

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-let _0x5a = (function(){
-    const _0x9 = s => s; // tiny indirection to obfuscate
-    const k1 = '\x70\x65\x72\x73\x6f\x6e';
-    const k2 = '\x61\x67\x65';
-    const v1 = '\x48\x75\x6d\x61\x6e';  
-    const v2 = '\x30';                     
-    const o = {};
-    o[_0x9(k1)] = v1;
-    o[_0x9(k2)] = v2;
-    return o;
-})();
-document.getElementById('result').innerHTML = _0x5a;
+
+const person = { age: 0 };
+
+function celebrateBirthday(p) {
+    p.age = (p.age || 0) + 1;
+    return p;
+}
+
+document.getElementById('result').innerHTML = JSON.stringify(person);
