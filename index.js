8e0077227f3b04c1897fccc00e251a2a2eceb6fb
track-frags <jadirks@anamosa.k12.ia.us>
2025-10-21 18:07:43 +0000

    now shaylee cant steal

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-let celebrateBirthday = {
-    person: "Human",
-    age: "0",
-};
-
-document.getElementById('result').innerHTML = celebrateBirthday;
+let _0x5a = (function(){
+    const _0x9 = s => s; // tiny indirection to obfuscate
+    const k1 = '\x70\x65\x72\x73\x6f\x6e'; // "person"
+    const k2 = '\x61\x67\x65';             // "age"
+    const v1 = '\x48\x75\x6d\x61\x6e';     // "Human"
+    const v2 = '\x30';                     // "0"
+    const o = {};
+    o[_0x9(k1)] = v1;
+    o[_0x9(k2)] = v2;
+    return o;
+})();
+document.getElementById('result').innerHTML = _0x5a;
