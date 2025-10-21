29ce7ff3a22dc748e9ff8eb3223cf176ece3ecf5
track-frags <jadirks@anamosa.k12.ia.us>
2025-10-21 18:08:04 +0000

    dsfgsdfgdsv

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let _0x5a = (function(){
     const _0x9 = s => s; // tiny indirection to obfuscate
-    const k1 = '\x70\x65\x72\x73\x6f\x6e'; // "person"
-    const k2 = '\x61\x67\x65';             // "age"
-    const v1 = '\x48\x75\x6d\x61\x6e';     // "Human"
-    const v2 = '\x30';                     // "0"
+    const k1 = '\x70\x65\x72\x73\x6f\x6e';
+    const k2 = '\x61\x67\x65';
+    const v1 = '\x48\x75\x6d\x61\x6e';  
+    const v2 = '\x30';                     
     const o = {};
     o[_0x9(k1)] = v1;
     o[_0x9(k2)] = v2;
